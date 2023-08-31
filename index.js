8ef80baeb8b3e01c1f330edeabc22344700a4404
Valerio Vaccaro <valerio@blockstream.com>
2023-08-31 12:54:13 +0200

    fix baudrate for jade_1.0.21_m5stickcplus

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ connectButton.onclick = async () => {
 
   var baudrate = 921600;
 
-  if (diymodelsel.value == "jade_0.1.48_m5stickcplus") || (diymodelsel.value == "jade_1.0.21_m5stickcplus"){
+  if (["jade_0.1.48_m5stickcplus", "jade_1.0.21_m5stickcplus"].includes(diymodelsel.value){
       baudrate = 115200;
   }
 
