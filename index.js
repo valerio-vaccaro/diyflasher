e182458a124d9e5add257f10781c6ba1a616c039
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-05-03 10:04:45 +0200

    fix han

--- index.js
+++ index.js
@@ -182,6 +182,35 @@ connectButtonNerd.onclick = async () => {
     {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
   ]; 
   
+  if (["han_1.6.4RC1_m5stack"].includes(diymodelselNerd.value)) { // han
+    addressesAndFiles = [
+      {address: '0x1000', fileName: '0x1000_bootloader.bin', progressBar: btprogressBar},
+      {address: '0x8000', fileName: '0x8000_partitions.bin', progressBar: ptprogressBar},
+      {address: '0xE000', fileName: '0xe000_boot_app0.bin', progressBar: otaprogressBar},
+      {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
+    ];
+  } else if (["han_1.6.4RC1_wt32-sc01", "han_1.6.4RC1_wt32-sc01-plus"].includes(diymodelselNerd.value)) { // han2
+    addressesAndFiles = [
+      {address: '0x1000', fileName: '0x1000_bootloader.bin', progressBar: btprogressBar},
+      {address: '0x8000', fileName: '0x8000_partition-table.bin', progressBar: ptprogressBar},
+      {address: '0xE000', fileName: '0xe000_ota_data_initial.bin', progressBar: otaprogressBar},
+      {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
+    ];
+  } else if (["nerdminer2_1.6.2_ESP32-2432S028R", "nerdminer2_1.6.3_ESP32-2432S028R"].includes(diymodelselNerd.value)) { // han2
+    addressesAndFiles = [
+      {address: '0x1000', fileName: '0x1000_bootloader.bin', progressBar: btprogressBar},
+      {address: '0x8000', fileName: '0x8000_partition-table.bin', progressBar: ptprogressBar},
+      {address: '0xE000', fileName: '0xe000_ota_data_initial.bin', progressBar: otaprogressBar},
+      {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
+    ];
+  } else if (["nerdminer2_1.6.2_esp32wroom", "nerdminer2_1.6.3_esp32wroom"].includes(diymodelselNerd.value)) { // han2
+    addressesAndFiles = [
+      {address: '0x1000', fileName: '0x1000_bootloader.bin', progressBar: btprogressBar},
+      {address: '0x8000', fileName: '0x8000_partition-table.bin', progressBar: ptprogressBar},
+      {address: '0xE000', fileName: '0xe000_ota_data_initial.bin', progressBar: otaprogressBar},
+      {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
+    ];
+  }
   let fileArray = [];
 
   for (const item of addressesAndFiles) {
