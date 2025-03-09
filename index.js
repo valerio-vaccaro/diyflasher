681be4e6896fc71dac8b519163412a6a1b72f429
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2025-03-09 17:53:32 +0100

    add 1.6.5RC1 for han boards

--- index.js
+++ index.js
@@ -202,14 +202,14 @@ connectButtonNerd.onclick = async () => {
       {address: '0xE000', fileName: '0xe000_boot_app0.bin', progressBar: otaprogressBar},
       {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
     ];
-  } else if (["han_1.6.4RC1_wt32-sc01"].includes(diymodelselNerd.value)) { // han2
+  } else if (["han_1.6.4RC1_wt32-sc01", "han_1.6.5RC1_wt32-sc01"].includes(diymodelselNerd.value)) { // han2
     addressesAndFiles = [
       {address: '0x1000', fileName: '0x1000_bootloader.bin', progressBar: btprogressBar},
       {address: '0x8000', fileName: '0x8000_partitions.bin', progressBar: ptprogressBar},
       {address: '0xE000', fileName: '0xe000_boot_app0.bin', progressBar: otaprogressBar},
       {address: '0x10000', fileName: '0x10000_firmware.bin', progressBar: firmwareprogressBar},
     ];
-  } else if (["han_1.6.4RC1_wt32-sc01-plus"].includes(diymodelselNerd.value)) { // han2
+  } else if (["han_1.6.4RC1_wt32-sc01-plus", "han_1.6.5RC1_wt32-sc01-plus"].includes(diymodelselNerd.value)) { // han2
     addressesAndFiles = [
       {address: '0x0000', fileName: '0x0000_bootloader.bin', progressBar: btprogressBar},
       {address: '0x8000', fileName: '0x8000_partitions.bin', progressBar: ptprogressBar},
